f9b7a0fd0bfb21602d718157bfc6d7c65a30b81f
Ivan Yakimov <edlinsoft@gmail.com>
2014-09-03 17:45:41 +0400

    Application is always run with administrator privileges

--- PathEditor/App.xaml.cs
+++ PathEditor/App.xaml.cs
@@ -1,11 +1,31 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Security.Principal;
+using System.Windows;
 
 namespace PathEditor
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (IsAdministrator() == false)
+            {
+                // Restart program and run as admin
+                var exeName = Process.GetCurrentProcess().MainModule.FileName;
+                var startInfo = new ProcessStartInfo(exeName) {Verb = "runas"};
+                Process.Start(startInfo);
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        private static bool IsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
     }
 }
--- PathEditor/Views/MainWindow.xaml.cs
+++ PathEditor/Views/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Windows;
-using PathEditor.Models;
+﻿using PathEditor.Models;
 using PathEditor.ModelViews;
 
 namespace PathEditor.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
