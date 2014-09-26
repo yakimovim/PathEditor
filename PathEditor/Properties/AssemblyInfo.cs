2b2c1fd39c72cb2483e7e77dabf9f0cd12c9445d
Ivan Yakimov <edlinsoft@gmail.com>
2014-09-26 10:56:33 +0400

    Assembly description was added

--- PathEditor/Properties/AssemblyInfo.cs
+++ PathEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Path Editor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Editor of environment variable PATH")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EdlinSoftware")]
 [assembly: AssemblyProduct("Path Editor")]
